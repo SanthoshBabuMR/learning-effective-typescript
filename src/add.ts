f3572fa2845af413f335f42462bbb32ea6ca51e0
Babu, Santhosh <BabuSa@Optum.com>
2023-03-21 07:03:16 +0530

    updated

--- src/add.ts
+++ src/add.ts
@@ -1,7 +1,14 @@
-// ./node_modules/.bin/tsc ./src/add.ts  -> compiles without errors
-// ./node_modules/.bin/tsc --noImplicitAny ./src/add.ts --allowJs  -> errors
-// with tsc, use --allowJs, to work '.js' extension
 function add(a, b) {
   return a + b;
 }
 add(10, null);
+/*
+ * ./node_modules/.bin/tsc ./src/add.ts --noEmit
+ * - compiles without errors
+ *
+ * ./node_modules/.bin/tsc --noImplicitAny ./src/add.ts --noEmit
+ * - errors
+ *
+ * with tsc, use --allowJs, to work '.js' extension
+ *
+ **/
