b2ce9393d9561bdaacb99b19e9f3ff1f6928572c
Babu, Santhosh <BabuSa@Optum.com>
2023-03-22 06:31:07 +0530

    updated

---
+++ src/Understand That Code Generation Is Independent of Types/cat.ts
@@ -0,0 +1,8 @@
+let x = "hello";
+x = 1234;
+
+/*
+ * ./node_modules/.bin/tsc src/Understand\ That\ Code\ Generation\ Is\ Independent\ of\ Types/cat.ts
+ * - throws errors but then also generates cat.js file
+ *
+ **/
